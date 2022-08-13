ac30510222965fa844a7c98f1839217664c5e119
renato <renato.hermoza@pucp.edu.pe>
2022-08-13 16:20:26 -0500

    add cauchy point

--- src/bin/02-rosenbrock.rs
+++ src/bin/02-rosenbrock.rs
@@ -1,5 +1,3 @@
-use std::time::Duration;
-
 use argmin::{
     core::{
         observers::{ObserverMode, SlogLogger},
@@ -11,24 +9,33 @@ use argmin::{
             condition::ArmijoCondition, BacktrackingLineSearch, HagerZhangLineSearch,
             MoreThuenteLineSearch,
         },
+        trustregion::{CauchyPoint, TrustRegion},
     },
 };
 use argmin_exploring::Rosenbrock;
+use std::time::Duration;
 use tabled::{Style, Table, Tabled};
 
 #[derive(Tabled)]
 struct Result {
+    family: String,
     method: String,
     best_cost: f64,
     time: String,
 }
 
 impl Result {
-    fn new(method: impl ToString, best_cost: f64, time: Option<Duration>) -> Self {
+    fn new(
+        family: impl ToString,
+        method: impl ToString,
+        best_cost: f64,
+        time: Option<Duration>,
+    ) -> Self {
         let time = time
             .map(|d| format!("{d:?}"))
             .unwrap_or_else(|| "-".to_string());
         Self {
+            family: family.to_string(),
             method: method.to_string(),
             best_cost,
             time,
@@ -37,13 +44,20 @@ impl Result {
 }
 
 fn main() {
+    let iterations = std::env::args()
+        .nth(1)
+        .map(|x| {
+            x.parse()
+                .unwrap_or_else(|x| panic!("Invalid number for `max_iters`: {x}"))
+        })
+        .unwrap_or(10);
+
     println!("Line solver methods");
     let problem = Rosenbrock::default();
     let init_param = vec![10.2, -20.0];
-    let iterations = 10;
     let mut results = Vec::new();
 
-    // Backtracking
+    // Linear search - Backtracking
     let backtracking = BacktrackingLineSearch::new(ArmijoCondition::new(0.0001).unwrap());
     let backtracking_solver = SteepestDescent::new(backtracking);
     let backtracking_res = Executor::new(problem, backtracking_solver)
@@ -53,12 +67,13 @@ fn main() {
         .unwrap();
     println!("Backtracking: {backtracking_res}");
     results.push(Result::new(
+        "Linear search",
         "Backtracking",
         backtracking_res.state.get_best_cost(),
         backtracking_res.state.get_time(),
     ));
 
-    // More-Thuente
+    // Linear search - More-Thuente
     let morethuente = MoreThuenteLineSearch::new();
     let morethuente_solver = SteepestDescent::new(morethuente);
     let morethuente_res = Executor::new(problem, morethuente_solver)
@@ -68,26 +83,45 @@ fn main() {
         .unwrap();
     println!("More-Thuente: {morethuente_res}");
     results.push(Result::new(
+        "Linear search",
         "More-Thuente",
         morethuente_res.state.get_best_cost(),
         morethuente_res.state.get_time(),
     ));
 
-    // Hager-Zhang
+    // Linear search - Hager-Zhang
     let hagerzhang = HagerZhangLineSearch::new();
     let hagerzhang_solver = SteepestDescent::new(hagerzhang);
     let hagerzhang_res = Executor::new(problem, hagerzhang_solver)
         .add_observer(SlogLogger::term(), ObserverMode::Always)
-        .configure(|state| state.param(init_param).max_iters(iterations))
+        .configure(|state| state.param(init_param.clone()).max_iters(iterations))
         .run()
         .unwrap();
     println!("Hager-Zhang: {hagerzhang_res}");
     results.push(Result::new(
+        "Linear search",
         "Hager-Zhang",
         hagerzhang_res.state.get_best_cost(),
         hagerzhang_res.state.get_time(),
     ));
 
+    // Trust Region - Cauchy Point
+    let cauchy_point = CauchyPoint::new();
+    let cauchy_point_solver = TrustRegion::new(cauchy_point);
+    let cauchy_point_res = Executor::new(problem, cauchy_point_solver)
+        .add_observer(SlogLogger::term(), ObserverMode::Always)
+        .configure(|state| state.param(init_param).max_iters(iterations))
+        .run()
+        .unwrap();
+    println!("Cauchy-Point: {cauchy_point_res}");
+    results.push(Result::new(
+        "Trust region",
+        "Cauchy-Point",
+        cauchy_point_res.state.get_best_cost(),
+        cauchy_point_res.state.get_time(),
+    ));
+
+    // Results table
     let table = Table::new(results).with(Style::modern()).to_string();
-    println!("Results:\n{table}");
+    println!("Results using {iterations} iterations:\n{table}");
 }
