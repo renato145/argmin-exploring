5ef70369864a6552465e5bf9d66d44c5c9768044
renato <renato.hermoza@pucp.edu.pe>
2022-08-14 21:35:22 -0500

    add all methods on argmin

--- src/bin/02-rosenbrock.rs
+++ src/bin/02-rosenbrock.rs
@@ -6,16 +6,20 @@ use argmin::{
     solver::{
         conjugategradient::{beta::PolakRibiere, NonlinearConjugateGradient},
         gradientdescent::SteepestDescent,
+        landweber::Landweber,
         linesearch::{
             condition::ArmijoCondition, BacktrackingLineSearch, HagerZhangLineSearch,
             MoreThuenteLineSearch,
         },
+        neldermead::NelderMead,
         newton::{Newton, NewtonCG},
+        particleswarm::ParticleSwarm,
         quasinewton::{SR1TrustRegion, BFGS, DFP, LBFGS},
+        simulatedannealing::SimulatedAnnealing,
         trustregion::{CauchyPoint, Steihaug, TrustRegion},
     },
 };
-use argmin_exploring::Rosenbrock;
+use argmin_exploring::{RosenbrockND, RosenbrockVec};
 use ndarray::{array, Array2};
 use std::time::Duration;
 use tabled::{Style, Table, Tabled};
@@ -71,14 +75,15 @@ fn main() {
         })
         .unwrap_or(10);
 
-    let problem = Rosenbrock::default();
     let init_param = array![10.2, -20.0];
+    let problem = RosenbrockND::default();
+    let problem_vec = RosenbrockVec::default();
     let mut results = Vec::new();
 
     // Linear search - Backtracking
     let backtracking = BacktrackingLineSearch::new(ArmijoCondition::new(0.0001).unwrap());
     let backtracking_solver = SteepestDescent::new(backtracking);
-    let backtracking_res = Executor::new(problem, backtracking_solver)
+    let backtracking_res = Executor::new(problem.clone(), backtracking_solver)
         .add_observer(SlogLogger::term(), ObserverMode::Every(log_every))
         .configure(|state| state.param(init_param.clone()).max_iters(iterations))
         .run()
@@ -96,7 +101,7 @@ fn main() {
     // Linear search - More-Thuente
     let morethuente = MoreThuenteLineSearch::new();
     let morethuente_solver = SteepestDescent::new(morethuente);
-    let morethuente_res = Executor::new(problem, morethuente_solver)
+    let morethuente_res = Executor::new(problem.clone(), morethuente_solver)
         .add_observer(SlogLogger::term(), ObserverMode::Every(log_every))
         .configure(|state| state.param(init_param.clone()).max_iters(iterations))
         .run()
@@ -114,7 +119,7 @@ fn main() {
     // Linear search - Hager-Zhang
     let hagerzhang = HagerZhangLineSearch::new();
     let hagerzhang_solver = SteepestDescent::new(hagerzhang);
-    let hagerzhang_res = Executor::new(problem, hagerzhang_solver)
+    let hagerzhang_res = Executor::new(problem.clone(), hagerzhang_solver)
         .add_observer(SlogLogger::term(), ObserverMode::Every(log_every))
         .configure(|state| state.param(init_param.clone()).max_iters(iterations))
         .run()
@@ -132,7 +137,7 @@ fn main() {
     // Trust Region - Cauchy Point
     let cauchy_point = CauchyPoint::new();
     let cauchy_point_solver = TrustRegion::new(cauchy_point);
-    let cauchy_point_res = Executor::new(problem, cauchy_point_solver)
+    let cauchy_point_res = Executor::new(problem.clone(), cauchy_point_solver)
         .add_observer(SlogLogger::term(), ObserverMode::Every(log_every))
         .configure(|state| state.param(init_param.clone()).max_iters(iterations))
         .run()
@@ -151,7 +156,7 @@ fn main() {
     // // Trust Region - Dogleg
     // let dogleg = Dogleg::new();
     // let dogleg_solver = TrustRegion::new(dogleg);
-    // let dogleg_res = Executor::new(problem, dogleg_solver)
+    // let dogleg_res = Executor::new(problem.clone(), dogleg_solver)
     //     .add_observer(SlogLogger::term(), ObserverMode::Every(log_every))
     //     .configure(|state| state.param(init_param).max_iters(iterations))
     //     .run()
@@ -168,7 +173,7 @@ fn main() {
     // Trust Region - Steighaug
     let steighaug = Steihaug::new();
     let steighaug_solver = TrustRegion::new(steighaug);
-    let steighaug_res = Executor::new(problem, steighaug_solver)
+    let steighaug_res = Executor::new(problem.clone(), steighaug_solver)
         .add_observer(SlogLogger::term(), ObserverMode::Every(log_every))
         .configure(|state| state.param(init_param.clone()).max_iters(iterations))
         .run()
@@ -189,7 +194,7 @@ fn main() {
     let nlcg_solver = NonlinearConjugateGradient::new(linesearch, beta_method)
         .restart_iters(10)
         .restart_orthogonality(0.1);
-    let nlcg_res = Executor::new(problem, nlcg_solver)
+    let nlcg_res = Executor::new(problem.clone(), nlcg_solver)
         .add_observer(SlogLogger::term(), ObserverMode::Every(log_every))
         .configure(|state| state.param(init_param.clone()).max_iters(iterations))
         .run()
@@ -206,7 +211,7 @@ fn main() {
 
     // Newton - Newton's method
     let newton = Newton::new();
-    let newton_res = Executor::new(problem, newton)
+    let newton_res = Executor::new(problem.clone(), newton)
         .add_observer(SlogLogger::term(), ObserverMode::Every(log_every))
         .configure(|state| state.param(init_param.clone()).max_iters(iterations))
         .run()
@@ -224,7 +229,7 @@ fn main() {
     // Newton - Newton-CG method
     let linesearch = MoreThuenteLineSearch::new();
     let newton_cg = NewtonCG::new(linesearch);
-    let newton_cg_res = Executor::new(problem, newton_cg)
+    let newton_cg_res = Executor::new(problem.clone(), newton_cg)
         .add_observer(SlogLogger::term(), ObserverMode::Every(log_every))
         .configure(|state| state.param(init_param.clone()).max_iters(iterations))
         .run()
@@ -242,7 +247,7 @@ fn main() {
     // Quasi Newton - BFGS
     let linesearch = MoreThuenteLineSearch::new();
     let bfgs = BFGS::new(linesearch);
-    let bfgs_res = Executor::new(problem, bfgs)
+    let bfgs_res = Executor::new(problem.clone(), bfgs)
         .add_observer(SlogLogger::term(), ObserverMode::Every(log_every))
         .configure(|state| {
             state
@@ -266,7 +271,7 @@ fn main() {
     // Quasi Newton - DFP
     let linesearch = MoreThuenteLineSearch::new();
     let dfp = DFP::new(linesearch);
-    let dfp_res = Executor::new(problem, dfp)
+    let dfp_res = Executor::new(problem.clone(), dfp)
         .add_observer(SlogLogger::term(), ObserverMode::Every(log_every))
         .configure(|state| {
             state
@@ -290,7 +295,7 @@ fn main() {
     // Quasi Newton - L-BFGS
     let linesearch = MoreThuenteLineSearch::new();
     let lbfgs = LBFGS::new(linesearch, 5);
-    let lbfgs_res = Executor::new(problem, lbfgs)
+    let lbfgs_res = Executor::new(problem.clone(), lbfgs)
         .add_observer(SlogLogger::term(), ObserverMode::Every(log_every))
         .configure(|state| state.param(init_param.clone()).max_iters(iterations))
         .run()
@@ -310,7 +315,7 @@ fn main() {
     // Quasi Newton - SR1-Trust Region
     let subproblem = Steihaug::new();
     let sr1tr = SR1TrustRegion::new(subproblem);
-    let sr1tr_res = Executor::new(problem, sr1tr)
+    let sr1tr_res = Executor::new(problem.clone(), sr1tr)
         .add_observer(SlogLogger::term(), ObserverMode::Every(log_every))
         .configure(|state| state.param(init_param.clone()).max_iters(iterations))
         .run()
@@ -323,6 +328,74 @@ fn main() {
         sr1tr_res.state.get_time(),
         sr1tr_res.state.get_iter(),
         sr1tr_res.state.get_termination_reason(),
+    ));
+
+    // Landweber Iteration
+    let landweber = Landweber::new(0.001);
+    let landweber_res = Executor::new(problem.clone(), landweber)
+        .add_observer(SlogLogger::term(), ObserverMode::Every(log_every))
+        .configure(|state| state.param(init_param.clone()).max_iters(iterations))
+        .run()
+        .unwrap();
+    println!("landweber: {landweber_res}");
+    results.push(Result::new(
+        "",
+        "Landweber Iteration",
+        landweber_res.state.get_best_cost(),
+        landweber_res.state.get_time(),
+        landweber_res.state.get_iter(),
+        landweber_res.state.get_termination_reason(),
+    ));
+
+    // Nelder-Mead
+    let nelder_mead = NelderMead::new(vec![array![-1.0, 3.0], array![2.0, 1.5], array![2.0, -1.0]]);
+    let nelder_mead_res = Executor::new(problem.clone(), nelder_mead)
+        .add_observer(SlogLogger::term(), ObserverMode::Every(log_every))
+        .configure(|state| state.param(init_param.clone()).max_iters(iterations))
+        .run()
+        .unwrap();
+    println!("nelder_mead: {nelder_mead_res}");
+    results.push(Result::new(
+        "",
+        "Nelder-Mead",
+        nelder_mead_res.state.get_best_cost(),
+        nelder_mead_res.state.get_time(),
+        nelder_mead_res.state.get_iter(),
+        nelder_mead_res.state.get_termination_reason(),
+    ));
+
+    // Simulated Annealing
+    let simulated_annealing = SimulatedAnnealing::new(15.0).unwrap();
+    let simulated_annealing_res = Executor::new(problem.clone(), simulated_annealing)
+        .add_observer(SlogLogger::term(), ObserverMode::Every(log_every))
+        .configure(|state| state.param(init_param.clone()).max_iters(iterations))
+        .run()
+        .unwrap();
+    println!("simulated_annealing: {simulated_annealing_res}");
+    results.push(Result::new(
+        "",
+        "Simulated Annealing",
+        simulated_annealing_res.state.get_best_cost(),
+        simulated_annealing_res.state.get_time(),
+        simulated_annealing_res.state.get_iter(),
+        simulated_annealing_res.state.get_termination_reason(),
+    ));
+
+    // Particle swarm optimization
+    let particle_swarm = ParticleSwarm::new((vec![-5.0, -5.0], vec![5.0, 5.0]), 100);
+    let particle_swarm_res = Executor::new(problem_vec.clone(), particle_swarm)
+        .add_observer(SlogLogger::term(), ObserverMode::Every(log_every))
+        .configure(|state| state.max_iters(iterations))
+        .run()
+        .unwrap();
+    println!("particle_swarm: {particle_swarm_res}");
+    results.push(Result::new(
+        "",
+        "Particle Swarm",
+        particle_swarm_res.state.get_best_cost(),
+        particle_swarm_res.state.get_time(),
+        particle_swarm_res.state.get_iter(),
+        particle_swarm_res.state.get_termination_reason(),
     ));
 
     // Results table
--- src/rosenbrock_ndarray.rs
+++ src/rosenbrock_ndarray.rs
@@ -1,24 +1,43 @@
-use argmin::core::{CostFunction, Gradient, Hessian};
+use std::sync::{Arc, Mutex};
+
+use argmin::{
+    core::{CostFunction, Gradient, Hessian},
+    solver::simulatedannealing::Anneal,
+};
 use argmin_testfunctions::{rosenbrock_2d, rosenbrock_2d_derivative, rosenbrock_2d_hessian};
-use ndarray::{Array1, Array2};
+use ndarray::{array, Array1, Array2};
+use rand::{distributions::Uniform, Rng};
+use rand_xoshiro::{rand_core::SeedableRng, Xoshiro256PlusPlus};
 
 /// The rosenbrock function is defined as:
 /// $ f(x,y) = (a-x)^2 + b(y-x^2)^2 $
-#[derive(Debug, Clone, Copy)]
+#[derive(Debug, Clone)]
 pub struct RosenbrockND {
     a: f64,
     b: f64,
+    lower_bound: Array1<f64>,
+    upper_bound: Array1<f64>,
+    /// Random number generator. We use a `Arc<Mutex<_>>` here because `ArgminOperator` requires
+    /// `self` to be passed as an immutable reference. This gives us thread safe interior
+    /// mutability.
+    rng: Arc<Mutex<Xoshiro256PlusPlus>>,
 }
 
 impl RosenbrockND {
-    pub fn new(a: f64, b: f64) -> Self {
-        Self { a, b }
+    pub fn new(a: f64, b: f64, lower_bound: Array1<f64>, upper_bound: Array1<f64>) -> Self {
+        Self {
+            a,
+            b,
+            lower_bound,
+            upper_bound,
+            rng: Arc::new(Mutex::new(Xoshiro256PlusPlus::from_entropy())),
+        }
     }
 }
 
 impl Default for RosenbrockND {
     fn default() -> Self {
-        Self::new(1.0, 100.0)
+        Self::new(1.0, 100.0, array![-5.0, -5.0], array![5.0, 5.0])
     }
 }
 
@@ -48,6 +67,38 @@ impl Hessian for RosenbrockND {
     fn hessian(&self, param: &Self::Param) -> Result<Self::Hessian, argmin::core::Error> {
         let h = rosenbrock_2d_hessian(&param.to_vec(), self.a, self.b);
         Ok(Array2::from_shape_vec((2, 2), h)?)
+    }
+}
+
+impl Anneal for RosenbrockND {
+    type Param = Array1<f64>;
+    type Output = Array1<f64>;
+    type Float = f64;
+
+    fn anneal(
+        &self,
+        param: &Self::Param,
+        temp: Self::Float,
+    ) -> Result<Self::Output, argmin::core::Error> {
+        let mut param_n = param.clone();
+        let mut rng = self.rng.lock().unwrap();
+        let distr = Uniform::from(0..param.len());
+        // Perform modifications to a degree proportional to the current temperature `temp`.
+        for _ in 0..(temp.floor() as u64 + 1) {
+            // Compute random index of the parameter vector using the supplied random number
+            // generator.
+            let idx = rng.sample(distr);
+
+            // Compute random number in [0.1, 0.1].
+            let val = rng.sample(Uniform::new_inclusive(-0.1, 0.1));
+
+            // modify previous parameter value at random position `idx` by `val`
+            param_n[idx] += val;
+
+            // check if bounds are violated. If yes, project onto bound.
+            param_n[idx] = param_n[idx].clamp(self.lower_bound[idx], self.upper_bound[idx]);
+        }
+        Ok(param_n)
     }
 }
 
